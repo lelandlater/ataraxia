06ad89c5f6b64c9bfb265e44490389a66fd0ef04
Leland Later <lelandlater@gmail.com>
2017-09-17 16:20:32 -0700

    GUI, completing all necessar views.

--- src/gui/gui/views.py
+++ src/gui/gui/views.py
@@ -1,15 +1,33 @@
 from flask import render_template, make_response, url_for
 from htmlmin.minify import html_minify
-from . import util
+from gui import util
+
+NavBar=util.NavBar
+NavItem=util.NavItem
 
 def index():
-    nav = util.NavBar(start=[util.NavItem('http://google.com', 'API')])
-    footer = util.NavBar(middle=[util.NavItem(url_for('contact'), 'CONTACT')])
-    buttons = []
+    nav = NavBar(start=[NavItem('http://google.com', 'API')])
+    footer = NavBar(middle=[NavItem(url_for('contact'), 'CONTACT')])
+    buttons = [NavItem(url_for('info'), 'INFO'), NavItem(url_for('request_auth'), 'AUTH')]
     rendered_html = render_template('index.html', nav=nav, buttons=buttons, footer=footer, title='Cue: social playlist')
     return make_response(html_minify(rendered_html), 200)
 
 def contact():
-    nav = util.NavBar(end=[util.NavItem(url_for('index'), 'BACK')])
-    rendered_html = render_template('contact.html', nav=nav, title='Contact Cue')
+    items = [NavItem(url_for('index'), 'HOME')]
+    rendered_html = render_template('contact.html', items=items, title='Contact Cue')
+    return make_response(html_minify(rendered_html), 200)
+
+def info():
+    items = [NavItem(url_for('index'), 'HOME')]
+    rendered_html = render_template('info.html', items=items, title='Contact Cue')
+    return make_response(html_minify(rendered_html), 200)
+
+def request_auth():
+    items = [NavItem(url_for('index'), 'HOME')]
+    rendered_html = render_template('request_auth.html', items=items, title='Contact Cue')
+    return make_response(html_minify(rendered_html), 200)
+
+def access():
+    items = [NavItem(url_for('index'), 'HOME')]
+    rendered_html = render_template('access.html', items=items, title='Contact Cue')
     return make_response(html_minify(rendered_html), 200)
--- src/gui/run.py
+++ src/gui/run.py
@@ -1,6 +1,6 @@
+from flask import Flask
 import logging
 from gui import views
-from flask import Flask
 
 log = logging.getLogger()
 log.setLevel('DEBUG')
@@ -12,6 +12,9 @@ app = Flask(__name__, template_folder='gui/templates', static_folder='gui/static
 
 app.route('/')(views.index)
 app.route('/contact')(views.contact)
+app.route('/info')(views.info)
+app.route('/request_auth')(views.request_auth)
+app.route('/access')(views.access)
 
 if __name__=="__main__":
     app.run(port=9000, debug=True, use_reloader=False)
