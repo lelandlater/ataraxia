6f3e8c23f3c50f8ae7ad21c066587958ddee8719
Leland Later <lelandlater@gmail.com>
2018-01-07 14:43:05 -0800

    Custom error pages nginx.

---
+++ src/db/init/03-permissions-on-tables.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -o errexit
+
+main () {
+  grant_blog_permissions
+}
+
+grant_blog_permissions() {
+  psql -v ON_ERRPOR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    \c ataraxiadb;
+    GRANT ALL PRIVILEGES ON TABLE posts TO $ATARAXIA_DB_USER;
+EOSQL
+}
+
+main "$@"
